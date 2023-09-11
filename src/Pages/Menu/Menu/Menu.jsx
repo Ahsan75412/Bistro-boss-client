689752408b91685e0e3be1871d035168b53aa89f
Ahsan <ahsanpub16b@gmail.com>
2023-09-11 12:09:57 +0600

    custom hook created for each menu section like: food category, all food items in dynamic way

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,9 +1,10 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg';
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
 
 const Menu = () => {
+    
     return (
         <div>
             <Helmet>
@@ -11,13 +12,8 @@ const Menu = () => {
             </Helmet>
           
             <Cover img={menuImg} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
+           
+        
         </div>
     );
 };
