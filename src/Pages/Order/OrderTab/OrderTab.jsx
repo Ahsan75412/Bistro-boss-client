aabd7c3fb4486cae7c71b5e6155a49d3c1625ea1
Ahsan <ahsanpub16b@gmail.com>
2023-09-16 22:09:07 +0600

    add here cart icon from daisy ui and react icon , and added handle add to cart in consol initial here

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,18 +1,54 @@
 import FoodCard from "../../../components/FoodCard/FoodCard";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const OrderTab = ({items}) => {
-    return (
-        <div className='grid md:grid-cols-3 gap-10'>
-        {
-           items.map(item => <FoodCard
-            key={item._id}
-            item={item}
-            
-           ></FoodCard>) 
-        }
-      </div>
-    );
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+
+
+
+
+
+//TODO: implement pagination here?
+const OrderTab = ({ items }) => {
+
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+
+  };
+
+
+  return (
+    <div >
+
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className='grid md:grid-cols-3 gap-10'>
+            {
+              items.map(item => <FoodCard
+                key={item._id}
+                item={item}
+
+              ></FoodCard>)
+            }
+          </div>
+
+        </SwiperSlide>
+
+      </Swiper>
+    </div>
+  );
 };
 
 export default OrderTab;
