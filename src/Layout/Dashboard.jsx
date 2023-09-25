8f1147d2355aa68984aff604c542fd66fb7f855e
Ahsan <ahsanpub16b@gmail.com>
2023-09-25 20:51:21 +0600

    in dashboard shows all items , manage items , add add items here

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -21,9 +21,10 @@ const Dashboard = () => {
         <div className="drawer lg:drawer-open ">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
+                   {/* Page content here */}
+                   <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
                 <Outlet></Outlet>
-                {/* Page content here */}
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+              
 
             </div>
             <div className="drawer-side bg-[#D1A054]">
@@ -37,8 +38,8 @@ const Dashboard = () => {
 
                             {/* Admin side bar content here */}
                             <li><NavLink to="/dashboard/home"><FaHome></FaHome>Admin Home</NavLink></li>
-                            <li><NavLink to="/dashboard/reservations"><FaUtensils></FaUtensils> Add Items</NavLink></li>
-                            <li><NavLink to="/dashboard/history"><FaWallet></FaWallet>Manage Items</NavLink></li>
+                            <li><NavLink to="/dashboard/addItem"><FaUtensils></FaUtensils> Add an Item</NavLink></li>
+                            <li><NavLink to="/dashboard/manageitems"><FaWallet></FaWallet>Manage Items</NavLink></li>
                             <li><NavLink to="/dashboard/history"><FaBook></FaBook>Manage Bookings</NavLink></li>
                             <li><NavLink to="/dashboard/allusers"><FaUsers></FaUsers>All Users</NavLink></li>
 
